1d9cd347f3be8800af5c65ab127903c0592e10a2
Vasuvachhani <vasuvachhani2004@gmail.com>
2024-01-13 11:55:54 +0530

    mongo conn change

--- connection/conn.js
+++ connection/conn.js
@@ -1,2 +1,4 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Job_Recruitment_System").catch(error => handleError(error));
+// mongodb+srv://vasu:sT3zr470jCxQrKZQ@cluster0.08gbz9e.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect("mongodb+srv://vasu:sT3zr470jCxQrKZQ@cluster0.08gbz9e.mongodb.net/?retryWrites=true&w=majority/Job_Recruitment_System").catch(error => handleError(error));
+//mongodb://localhost:27017
