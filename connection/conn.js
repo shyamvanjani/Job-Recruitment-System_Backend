202199733ba9c5cd5a88ae6b74938a7d3fcd0bd2
Shyam <shyamvanjani6633@gmail.com>
2024-05-22 13:11:47 +0530

    first commit

--- connection/conn.js
+++ connection/conn.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
 // mongodb+srv://vasu:sT3zr470jCxQrKZQ@cluster0.08gbz9e.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://vasu:sT3zr470jCxQrKZQ@cluster0.08gbz9e.mongodb.net/?retryWrites=true&w=majority/Job_Recruitment_System").catch(error => handleError(error));
+mongoose.connect("mongodb+srv://tannmayhedau619:Tanmay%40619@cluster0.fw1xhuw.mongodb.net/Job_Recruitment_System").catch(error => handleError(error));
 //mongodb://localhost:27017
+//mongodb+srv://tannmayhedau619:Tanmay%40619@cluster0.fw1xhuw.mongodb.net/Job_Rec
